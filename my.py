e5e48066f7a5cb09bdb9770efc7072ad65d3f4e6
ramyalakshmi2862k2 <t.rlakshmi1@tcs.com>
2025-10-30 17:39:05 +0530

    na ippotha push panran

--- my.py
+++ my.py
@@ -1,26 +1,11 @@
-import random
+import json
 
-def guess_the_number():
-    """A simple 'Guess the Number' game."""
-    print("Welcome to Guess the Number!")
-    print("I'm thinking of a number between 1 and 100.")
-    secret_number = random.randint(1, 100)
-    attempts = 0
-
-    while True:
-        try:
-            guess = int(input("Enter your guess: "))
-            attempts += 1
-
-            if guess < secret_number:
-                print("Too low! Try again.")
-            elif guess > secret_number:
-                print("Too high! Try again.")
-            else:
-                print(f"Congratulations! You guessed the number {secret_number} in {attempts} attempts.")
-                break
-        except ValueError:
-            print("Invalid input. Please enter a whole number.")
-
-if __name__ == "__main__":
-    guess_the_number()
+print(json.dumps({"name": "John", "age": 30}))
+print(json.dumps(["apple", "bananas"]))
+print(json.dumps(("apple", "bananas")))
+print(json.dumps("hello"))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
